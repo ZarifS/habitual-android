4ca0afb036e1e486f4351d3872367acbc05f1e76
Zarif Shahriar <zshahria@ciena.com>
2017-04-24 14:40:17 -0400

    Addition of new Add Habit page

--- app/src/main/java/com/closedbracket/trackit/MainActivity.java
+++ app/src/main/java/com/closedbracket/trackit/MainActivity.java
@@ -1,7 +1,10 @@
 package com.closedbracket.trackit;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void addHabit(View view){
+        Log.i("Button Clicked","Bringing to next Page");
+        Intent intent = new Intent(this, AddHabit.class);
+        startActivity(intent);
     }
 }
