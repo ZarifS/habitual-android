c5a41c5b8e1e581e5622323d7c994909567c5460
Zarif Shahriar <zshahria@ciena.com>
2017-04-25 17:21:53 -0400

    Added switch and repeat

--- app/src/main/java/com/closedbracket/trackit/AddHabit.java
+++ app/src/main/java/com/closedbracket/trackit/AddHabit.java
@@ -2,6 +2,8 @@ package com.closedbracket.trackit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class AddHabit extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class AddHabit extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_habit);
+    }
+
+    public void repeatClick(View view){
+        Log.i("Repeat", "Bringing to next page");
     }
 }
