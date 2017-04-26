e10210840b7cc017b2fab94e01ae4a1882015c78
Zarif Shahriar <zshahria@ciena.com>
2017-04-26 16:40:01 -0400

    Added time to wheel, added switch functionality, added progress bar (wip)

--- app/src/main/java/com/closedbracket/trackit/AddHabit.java
+++ app/src/main/java/com/closedbracket/trackit/AddHabit.java
@@ -4,13 +4,34 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+
 
 public class AddHabit extends AppCompatActivity {
+
+    private Switch mySwitch;
+    private View time;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_habit);
+        time = findViewById(R.id.time_layout);
+        mySwitch = (Switch) findViewById(R.id.reminder_switch);
+        //attach a listener to check for changes in state
+        mySwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    time.setVisibility(View.VISIBLE);
+                }
+                else{
+                    time.setVisibility(View.GONE);
+                }
+            }
+        });
     }
 
     public void repeatClick(View view){
