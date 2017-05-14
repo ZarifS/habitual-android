9bd262c0b96aa01c9a253cbf8ddbaf7b23f3109a
Zarif Shahriar <Zarif@Zarifs-MacBook-Pro.local>
2017-05-13 23:12:10 -0400

    Realm reading working well, need to populate list views. Added new look to listView xml

--- app/src/main/java/com/closedbracket/trackit/MainActivity.java
+++ app/src/main/java/com/closedbracket/trackit/MainActivity.java
@@ -2,17 +2,29 @@ package com.closedbracket.trackit;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 
+import io.realm.Realm;
+import io.realm.RealmResults;
+
 public class MainActivity extends AppCompatActivity {
+
+    private RealmResults<Habit> habitsResults;
+    public Realm realm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Realm.init(this);
+        realm = Realm.getDefaultInstance();
+        getHabits();
+        for(Habit habit : habitsResults) {
+            Log.i("Habit name", habit.toString());
+        }
     }
 
     public void addHabit(View view){
@@ -27,5 +39,10 @@ public class MainActivity extends AppCompatActivity {
         if (resultCode == Activity.RESULT_OK) {
             Log.i("Return to Main Habit:","Got back data from AddHabit activity.");
         }
+    }
+
+    private void getHabits(){
+        habitsResults = realm.where(Habit.class).findAllSorted("name");
+        Log.i("Get Habits", "Got all habits");
     }
 }
