202389d88866794897bc391e6b4d5e935984d4ac
Zarif Shahriar <zarif.shahriar@gmail.com>
2017-11-09 21:24:06 -0500

    Added and implemented feedback with correct email

--- app/src/main/java/com/accentsoftware/habitual/Feedback.java
+++ app/src/main/java/com/accentsoftware/habitual/Feedback.java
@@ -1,13 +1,96 @@
 package com.accentsoftware.habitual;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.honorato.multistatetogglebutton.MultiStateToggleButton;
 
 public class Feedback extends AppCompatActivity {
+
+    private EditText message;
+    private MultiStateToggleButton subject;
+    private final String SUBJECT [] = {"Habitual:Bug","Habitual:Request", "Habitual:Comment"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feedback);
+        message = (EditText) findViewById(R.id.message);
+        subject = (MultiStateToggleButton) findViewById(R.id.feedbackSubject);
+        subject.setValue(0); //Default on bug subject
+    }
+
+    public void sendFeedbackClick(View view){
+        if (message.getText().toString().equals("")) {
+            Toast.makeText(this, "Please enter a message to send.", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            int val = subject.getValue();
+            new SendMail().execute( SUBJECT[val], message.getText().toString() );
+        }
+    }
+
+    private class SendMail extends AsyncTask<String, Void, Integer>
+    {
+        ProgressDialog pd = null;
+        String error = null;
+
+        @Override
+        protected void onPreExecute() {
+            // TODO Auto-generated method stub
+            super.onPreExecute();
+            pd = new ProgressDialog(Feedback.this);
+            pd.setTitle("Sending Mail");
+            pd.setMessage("Please wait...");
+            pd.setCancelable(false);
+            pd.show();
+        }
+
+        @Override
+        protected Integer doInBackground(String... params) {
+            // TODO Auto-generated method stub
+            GMailSender sender = new GMailSender("habitual.application@gmail.com", "HabitualApplication");
+
+            sender.setTo(new String[]{"zarif.shahriar@gmail.com"});
+            sender.setFrom("habitual.application@gmail.com");
+            sender.setSubject(params[0]);
+            sender.setBody(params[1]);
+            Log.i("doInBackground", params[0]);
+            try {
+                if(sender.send()) {
+                    System.out.println("Message sent");
+                    return 1;
+                } else {
+                    return 2;
+                }
+            } catch (Exception e) {
+                error = e.getMessage();
+                Log.e("SendMail", e.getMessage(), e);
+            }
+
+            return 3;
+        }
+
+        protected void onPostExecute(Integer result) {
+            pd.dismiss();
+            if(result==1) {
+                Toast.makeText(Feedback.this,
+                        "Feedback was sent successfully.", Toast.LENGTH_LONG)
+                        .show();
+            } else if(result==2) {
+                Toast.makeText(Feedback.this,
+                        "Feedback was not sent. Please try again later.", Toast.LENGTH_LONG).show();
+            } else if(result==3) {
+                Toast.makeText(Feedback.this,
+                        "There was a problem sending the feedback, please try again later.",
+                        Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
--- app/src/main/java/com/accentsoftware/habitual/GMailSender.java
+++ app/src/main/java/com/accentsoftware/habitual/GMailSender.java
@@ -6,108 +6,161 @@ package com.accentsoftware.habitual;
  * This code should not be modified.
  */
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.security.Security;
+import java.util.Date;
 import java.util.Properties;
 
+import javax.activation.CommandMap;
 import javax.activation.DataHandler;
 import javax.activation.DataSource;
-import javax.mail.Message;
+import javax.activation.FileDataSource;
+import javax.activation.MailcapCommandMap;
+import javax.mail.BodyPart;
+import javax.mail.Multipart;
 import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 
 public class GMailSender extends javax.mail.Authenticator {
 
-    private String mailhost = "smtp.gmail.com";
     private String user;
     private String password;
-    private Session session;
 
-    static {
-        Security.addProvider(new JSSEProvider());
+    private String [] to;
+    private String from;
+
+    private String port;
+    private String sport;
+
+    private String host;
+
+    private String subject;
+    private String body;
+
+    private boolean auth;
+    private boolean debuggable;
+
+    private Multipart multi;
+
+    public GMailSender(){
+        host = "smtp.gmail.com";
+        port = "465";
+        sport = "465";
+
+        user = "";
+        password = "";
+        from = "";
+        subject = "";
+        body = "";
+
+        debuggable = false;
+        auth = true;
+
+        multi = new MimeMultipart();
+
+        // There is something wrong with MailCap, javamail can not find a handler for the multipart/mixed part, so this bit needs to be added.
+        MailcapCommandMap mc = (MailcapCommandMap) CommandMap.getDefaultCommandMap();
+        mc.addMailcap("text/html;; x-java-content-handler=com.sun.mail.handlers.text_html");
+        mc.addMailcap("text/plain;; x-java-content-handler=com.sun.mail.handlers.text_plain");
+        mc.addMailcap("multipart/*;; x-java-content-handler=com.sun.mail.handlers.multipart_mixed");
+        mc.addMailcap("message/rfc822;; x-java-content-handler=com.sun.mail.handlers.message_rfc822");
+        CommandMap.setDefaultCommandMap(mc);
     }
 
-    public GMailSender(String user, String password) {
+    public GMailSender(String user, String password){
+        this();
         this.user = user;
         this.password = password;
-
-        Properties props = new Properties();
-        props.setProperty("mail.transport.protocol", "smtp");
-        props.setProperty("mail.host", mailhost);
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.port", "465");
-        props.put("mail.smtp.socketFactory.port", "465");
-        props.put("mail.smtp.socketFactory.class",
-                "javax.net.ssl.SSLSocketFactory");
-        props.put("mail.smtp.socketFactory.fallback", "false");
-        props.setProperty("mail.smtp.quitwait", "false");
-
-        session = Session.getDefaultInstance(props, this);
     }
 
-    protected PasswordAuthentication getPasswordAuthentication() {
+    public boolean send() throws Exception {
+        Properties props = setProperties();
+
+        try{
+            Session session = Session.getInstance(props, this);
+            session.setDebug(true);
+
+            MimeMessage msg = new MimeMessage(session);
+
+            msg.setFrom(new InternetAddress(from));
+
+            InternetAddress[] addressTo = new InternetAddress[to.length];
+            for(int i=0; i<to.length; i++){
+                addressTo[i] = new InternetAddress(to[i]);
+            }
+
+            msg.setRecipients(MimeMessage.RecipientType.TO, addressTo);
+            msg.setSubject(subject);
+            msg.setSentDate(new Date());
+
+            BodyPart messageBodyPart = new MimeBodyPart();
+            messageBodyPart.setText(body);
+            multi.addBodyPart(messageBodyPart);
+
+            msg.setContent(multi);
+
+            Transport transport = session.getTransport("smtps");
+            transport.connect(host, 465, user, password);
+            transport.sendMessage(msg, msg.getAllRecipients());
+            transport.close();
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public void addAttachment(String filename) throws Exception {
+        BodyPart messageBodyPart = new MimeBodyPart();
+        DataSource source = new FileDataSource(filename);
+        messageBodyPart.setDataHandler(new DataHandler(source));
+        messageBodyPart.setFileName(filename);
+
+        multi.addBodyPart(messageBodyPart);
+    }
+
+    @Override
+    public PasswordAuthentication getPasswordAuthentication() {
         return new PasswordAuthentication(user, password);
     }
 
-    public synchronized void sendMail(String subject, String body, String sender, String recipients) throws Exception {
-        try{
-            MimeMessage message = new MimeMessage(session);
-            DataHandler handler = new DataHandler(new ByteArrayDataSource(body.getBytes(), "text/plain"));
-            message.setSender(new InternetAddress(sender));
-            message.setSubject(subject);
-            message.setDataHandler(handler);
-            if (recipients.indexOf(',') > 0)
-                message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(recipients));
-            else
-                message.setRecipient(Message.RecipientType.TO, new InternetAddress(recipients));
-            Transport.send(message);
-        }catch(Exception e){
+    private Properties setProperties() {
+        Properties props = new Properties();
 
+        props.put("mail.smtp.host", host);
+
+        if(debuggable) {
+            props.put("mail.debug", "true");
         }
+
+        if(auth) {
+            props.put("mail.smtp.auth", "true");
+        }
+
+        props.put("mail.smtp.port", port);
+        props.put("mail.smtp.socketFactory.port", sport);
+        props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        props.put("mail.smtp.socketFactory.fallback", "false");
+
+        return props;
     }
 
-    public class ByteArrayDataSource implements DataSource {
-        private byte[] data;
-        private String type;
+    public void setTo(String[] toAddress) {
+        this.to = toAddress;
+    }
 
-        public ByteArrayDataSource(byte[] data, String type) {
-            super();
-            this.data = data;
-            this.type = type;
-        }
+    public void setFrom(String fromAddress) {
+        this.from = fromAddress;
+    }
 
-        public ByteArrayDataSource(byte[] data) {
-            super();
-            this.data = data;
-        }
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
 
-        public void setType(String type) {
-            this.type = type;
-        }
-
-        public String getContentType() {
-            if (type == null)
-                return "application/octet-stream";
-            else
-                return type;
-        }
-
-        public InputStream getInputStream() throws IOException {
-            return new ByteArrayInputStream(data);
-        }
-
-        public String getName() {
-            return "ByteArrayDataSource";
-        }
-
-        public OutputStream getOutputStream() throws IOException {
-            throw new IOException("Not Supported");
-        }
+    public void setBody(String body) {
+        this.body = body;
     }
 }
